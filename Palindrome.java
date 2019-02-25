2a91665eb9d82eefedd452eceb6b1ddcec2cb17c
githubUser <githubUser@githubPro.com>
2019-02-25 18:56:52 +0530

    Create Palindrome.java
    first commit code-4

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Palindrome{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;
+  int n;  
+   Scanner in = new Scanner(System.in);
+  
+  System.out.println("Enter the number");
+     n = in.nextInt(); 
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
